40730b19af913a4da496a2aab6528438b8f4a5e0
Ken Segami <ken.s.vollyball@gmail.com>
2019-08-26 10:36:09 +0000

    [Add]Routing of customer

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,28 @@ devise_for :customers, controllers: {
 #トップ画面
 	root 'products#index'
 
-
 #ユーザー側
+	resources :customers, only:[:show, :edit, :update] do
+		collection do
+			get :withdrawal
+			post '/delete' => 'customers#delete'
+			get :orderd
+		end
+	end
+
+	resources :client_addresses, only:[:new, :create, :edit, :update, :destroy]
+	resources :carts, only:[:index, :create, :edit, :update, :destroy]
+	resources :products, only:[:show]
+
+	resources :contacts, only:[:new, :create] do
+		collection do
+			post :confirm
+			get :complete
+		end
+	end
+
+
+
 
 #管理者側
 
