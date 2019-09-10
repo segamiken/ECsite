83e4e046f57694f544aa27a1df5c12a639200e9c
Ken Segami <ken.s.vollyball@gmail.com>
2019-09-10 15:07:48 +0000

    [Add] orderd html file

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
 	belongs_to :customer
 	has_many :order_products
 	accepts_nested_attributes_for :order_products, allow_destroy: true
+
+	enum payment_method:{クレジットカード: 0, 銀行振込: 1, コンビニ決済: 2}
 end
